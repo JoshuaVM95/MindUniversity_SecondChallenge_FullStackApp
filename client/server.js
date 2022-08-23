d4ed5cdc9b27f0ae12bebbb2005327e9a38348c1
JoshuaVM95 <joshuavm95@gmail.com>
2022-08-22 23:22:06 -0500

    fix(server): add rate limit

--- client/server.js
+++ client/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const path = require("path");
 
+// set up rate limiter: maximum of five requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = new RateLimit({
+	windowMs: 60 * 1000, // 1 minute
+	max: 5
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+// apply rate limiter to all requests
+app.use(limiter);
 
 app.use(express.static(path.join(__dirname, "build")));
 
